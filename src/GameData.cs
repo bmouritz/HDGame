9a3c261bfd250a6a50355be58fa5aac8a7bf9ef6
bmouritz <39233162+bmouritz@users.noreply.github.com>
2018-10-11 11:33:10 +1100

    Added GameData to play with singletons

---
+++ src/GameData.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyGame.src
+{
+    public class GameData
+    {
+        private static GameData instance;
+        private int _score;
+
+        private GameData()
+        {
+            _score = 0;
+        }
+
+        public static GameData Instance
+        {
+            get { if (instance == null) { instance = new GameData(); }
+                return instance;
+            }
+        }
+
+        public static int Score
+        {
+            get
+            {
+                return instance._score;
+            }
+
+            set
+            {
+                instance._score = value;
+            }
+        }
+
+        private void AddToScore(int AddScore)
+        {
+            instance._score += AddScore;
+        }
+
+        private void DecreaseScore(int AddScore)
+        {
+            instance._score -= AddScore;
+        }
+    }
+}
